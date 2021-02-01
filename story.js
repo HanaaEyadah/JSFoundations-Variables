e4667830697fc280d80439c93cd5e4a21988da54
hanaa eyadah <hanaa.eyadah@outlook.com>
2021-02-01 19:05:02 +0300

    Task Done

---
+++ story.js
@@ -0,0 +1,14 @@
+
+let myName = "hanaa";
+let age = 27;
+let hobby = "gaming";
+
+let age2 = 33;
+
+let task = (`Mrs. ${myName}, in ${age2} years, you will be ${age2+age} 
+years old. By then you will have to decided of ${hobby} practice`);
+
+console.log(task);
+
+
+
